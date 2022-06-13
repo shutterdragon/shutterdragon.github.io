c091fdd1f4cdcd59b693ea2d234fb0c5cbe6f078
Eunice Dhivya <divi.seed@gmail.com>
2022-06-13 15:24:38 +0530

    portfolio page responsive styling

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -3,10 +3,12 @@ import Star from "../img/Star.png";
 function PortfolioItem({ item }) {
   return (
     <div className="portfolioItem">
-      <img className="star" src={Star} alt="" />
-      <div className="titlePic">
-        <h2>{item.title}</h2>
-        <img src="../img/thumbnail.jpeg" alt="Mars Mission" />
+      <div className="titleWStar">
+        <img className="star" src={Star} alt="" />
+        <div className="titlePic">
+          <h2>{item.title}</h2>
+          <img src="../img/thumbnail.jpeg" alt="Mars Mission" />
+        </div>
       </div>
       <div className="publicationName">{item.publication}</div>
       <div className="description">{item.description}</div>
